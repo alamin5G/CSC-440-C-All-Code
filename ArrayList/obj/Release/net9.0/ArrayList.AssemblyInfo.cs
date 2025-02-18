4a0836e64cd64625f20c3c1f6dc8828fa6c5d739
Md Al Amin <alaminrakib4864@gmail.com>
2025-02-18 18:33:44 +0600

    testing c# file

--- ArrayList/obj/Release/net9.0/ArrayList.AssemblyInfo.cs
+++ ArrayList/obj/Release/net9.0/ArrayList.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayList")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c49c4dad83ce2275e292e7e4a0beb5c389ffb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayList")]
