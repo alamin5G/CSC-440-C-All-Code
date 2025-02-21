b23e81e7e5e4dd1ada406f4ba083e71f245f3093
Md Al Amin <alaminrakib4864@gmail.com>
2025-02-21 23:52:18 +0600

    taking array input from the user

--- MyFirstLab/MyClass.cs
+++ MyFirstLab/MyClass.cs
@@ -8,26 +8,76 @@ namespace MyFirstLab
     public class MyClass
 
     {
-        string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "Toyota" };
-        int[] myNum = { 10, 20, 30, 40, 50 };
+        
         static void Main(string[] args)
         {
-            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 }, {9, 0, 2} };
+           jaggedArray();
+            
+        }
 
-            Console.WriteLine(numbers.Length);
-            Console.WriteLine(numbers.Rank);
-            Console.WriteLine(numbers.GetLength(0));
-            Console.WriteLine(numbers.GetLength(1));
+        private static void singleArray(){
+             Console.Write("Enter the array size: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Enter the array elements: ");
+            for(int i=0; i<size; i++){
 
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-        
+            Console.WriteLine("The array elements are: ");
+            foreach(int i in arr){
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
 
+        private static void doubleArray(){
+            Console.Write("Enter the row size: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[row, row];
+            Console.WriteLine("Enter the array elements: ");
+            for(int i= 0; i< arr.GetLength(0); i++){
+                for(int j = 0; j< arr.GetLength(1); j++){
+                    Console.Write("Enter the element at position [" + i + ", " + j + "]: ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
 
-           for(int i = 0; i< numbers.GetLength(0); i++)
-            {
-                for(int j = 0; j< numbers.GetLength(1); j++)
-                {
-                    Console.Write(numbers[i, j] + " ");
+            Console.WriteLine("The array elements are: ");
+            for(int i = 0; i< arr.GetLength(0); i++){
+                for(int j = 0; j < arr.GetLength(1); j++){
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        private static void jaggedArray(){
+            Console.Write("Enter the row size: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            int[][] arr = new int[row][];
+            Console.WriteLine("Now set the colum size for each row: ");
+           
+            for(int i = 0; i<arr.Length; i++){  
+            Console.Write("Enter the column size arr[" + i + "]: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            arr[i] = new int[col];	
+            }
+
+            Console.WriteLine("Enter the array elements: ");
+            for(int i = 0; i < arr.Length; i++){
+                for(int j = 0; j < arr[i].Length; j++){
+                    Console.Write("Enter the element at [" + i + ", " + j + "]: ");
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The array elements are: ");
+            for(int i = 0; i < arr.Length; i++){
+                for(int j = 0; j < arr[i].Length; j++){
+                    Console.Write(arr[i][j] + " ");
                 }
                 Console.WriteLine();
             }
--- MyFirstLab/obj/Debug/net9.0/MyFirstLab.AssemblyInfo.cs
+++ MyFirstLab/obj/Debug/net9.0/MyFirstLab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c017d93cc476cfc88e7979c45bbb13b1bc0508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be12a2693e340f573474c9d607b5a39ce7c7bf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
