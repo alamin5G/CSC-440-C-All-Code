4c554579013eb36ff0e80f48f75567dc65c4ebf6
Md Al Amin <21303134@iubat.edu>
2025-02-27 17:29:44 +0600

    practicing C#

---
+++ OnlineVotingAPI/OnlineVotingAPI/Models/Election.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineVotingAPI.Models
+{
+    public class Election
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ICollection<Voter> Voters { get; set; }
+        public ICollection<Result> Results { get; set; }
+    }
+}
---
+++ OnlineVotingAPI/OnlineVotingAPI/Models/Voter.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineVotingAPI.Models
+{
+    public class Voter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VoterId { get; set; } // Unique identifier
+        public int ElectionId { get; set; }
+        public Election Election { get; set; }
+        public int? VotedForCandidateId { get; set; } // Nullable, as they may not have voted yet
+    }
+}
